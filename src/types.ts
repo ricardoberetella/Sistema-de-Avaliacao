e46603118d1e4cc6ddf5c5dcb9ea6cf12a038993
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-01 15:58:39 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,20 @@
 export type TurmaId = 'MA' | 'MB' | 'TA' | 'TB';
+export type UCId = 'FUSI' | 'CRD';
+export type NivelDesempenho = 'NEA' | 'APO' | 'PAR' | 'AUT';
 
-export interface OperacaoUsinagem {
+export interface CapacidadeTecnica {
   id: string;
-  numero: string; // Ex: "OP. 01"
-  titulo: string; // Ex: "FACEAR NO TORNO"
-  maquina: 'TORNO' | 'FRESA' | 'FURADEIRA' | 'BANCADA';
+  ucId: UCId;
+  codigo: string; // Ex: "CAP. 01"
+  descricao: string;
 }
 
 export interface Aluno {
   id: string;
   nome: string;
   turmaId: TurmaId;
-  operacoesConcluidas: string[]; // IDs das operações validadas
+  // Mapeia o ID da capacidade para o nível alcançado
+  avaliacoes: {
+    [capacidadeId: string]: NivelDesempenho;
+  };
 }
