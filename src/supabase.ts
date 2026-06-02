68395834aed5b022d3add3b48c10f5094dc88f2b
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-02 12:58:44 -0300

    Delete src/supabase.ts

--- src/supabase.ts
+++
@@ -1,8 +0,0 @@
-// src/supabase.ts
-import { createClient } from '@supabase/supabase-client';
-
-// Puxa as variáveis de ambiente configuradas na Netlify
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
