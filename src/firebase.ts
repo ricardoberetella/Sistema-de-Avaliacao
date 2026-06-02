dc22ecb357fc15cf39ad3a6545ca057da33e6352
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-02 10:43:28 -0300

    Update and rename firebase.js to firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Conexão com o banco de dados
+import { getFirestore } from "firebase/firestore";
 
-// Configuração que puxa os dados com segurança da nuvem
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,11 +11,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-// Exporta o banco de dados para ser usado em outras telas
-export const db = getFirestore(app); 
-
+// Exporta a conexão do banco em nuvem
+export const db = getFirestore(app);
 export default app;
