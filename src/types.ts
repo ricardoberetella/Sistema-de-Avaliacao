ecee82609b1c4663ba989008a0ca9e632e00e456
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-02 16:52:19 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
 // src/types.ts
-
 export type TurmaId = 'MA' | 'MB' | 'TA' | 'TB';
-export type UCId = 'FUSI' | 'CRD' | 'LIDT'| 'CMAT';
+export type UCId = 'FUSI' | 'CRD' | 'LIDT' | 'CMAT';
+
 export type NivelDesempenho = 'NSA' | 'APO' | 'PAR' | 'AUT';
 
 export interface CapacidadeTecnica {
   id: string;
+  ucId: UCId;
   codigo: string;
   descricao: string;
-  ucId: UCId;
 }
 
 export interface Aluno {
