aec1708bc4328cde76b19601993ac99781f9ddb3
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-07 16:22:42 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 // src/types.ts
 export type TurmaId = 'MA' | 'MB' | 'TA' | 'TB';
-export type UCId = 'FUSI' | 'CRD' | 'LIDT' | 'CMAT';
-export type NivelDesempenho = 'NSA' | 'APO' | 'PAR' | 'AUT';
+export type UCId = 'FUSI' | 'CRD' | 'LIDT' | 'CIEMA'; // Alterado de CMAT para CIEMA para bater com o App.tsx
 
 export interface CapacidadeTecnica {
   id: string;
@@ -14,10 +13,15 @@ export interface Aluno {
   id: string;
   nome: string;
   turmaId: TurmaId;
-  avaliacoes: {
+  avaliacoes?: {                  // Adicionado o ? para torná-lo opcional e evitar erros se o Firestore retornar vazio
     [capacidadeId: string]: NivelDesempenho;
   };
-  observacoes: {
+  observacoes?: {                 // Adicionado o ?
+    [capacidadeId: string]: string;
+  };
+  notasNumericas?: {              // ESSA LINHA FOI ADICIONADA PARA CURAR O ERRO DA BUILD!
     [capacidadeId: string]: string;
   };
 }
+
+export type NivelDesempenho = 'NSA' | 'APO' | 'PAR' | 'AUT';
