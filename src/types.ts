11bc03d6b4cda31d2d05c5721d42d477d6e3e66e
ricardoberetella <ricardoberetella20@gmail.com>
2026-06-23 11:15:28 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,8 @@
 // src/types.ts
 
 export type TurmaId = 'MA' | 'MB' | 'TA' | 'TB';
-export type UCId = 'FUSI' | 'CRD' | 'LIDT' | 'CIEMA';
+// Atualizado: 'MAP' incluído na lista de Unidades Curriculares
+export type UCId = 'FUSI' | 'CRD' | 'LIDT' | 'CIEMA' | 'MAP';
 
 export interface CapacidadeTecnica {
   id: string;
